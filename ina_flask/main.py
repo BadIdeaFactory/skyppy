bf7f0aa8a652ec777484ba4adb30365518f9f5a4
Laurian Gridinoc <laurian@gridinoc.name>
2021-07-04 12:41:13 +0200

    CORS

--- ina_flask/main.py
+++ ina_flask/main.py
@@ -1,6 +1,16 @@
 from __future__ import unicode_literals
 import os
-from flask import Flask, render_template, send_file, request, jsonify, make_response, url_for, Response
+from flask import (
+    Flask,
+    render_template,
+    send_file,
+    request,
+    jsonify,
+    make_response,
+    url_for,
+    Response,
+)
+from flask_cors import CORS
 from ina_cache import Cache
 import pandas as pd
 import re
@@ -9,19 +19,21 @@ import json
 from subprocess import call
 from skyppy_core import Segment, Skyppy_flask
 
-#initialize flask
+# initialize flask
 app = Flask(__name__)
-app.static_url_path=app.config.get('video')
+app.static_url_path = app.config.get("video")
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
-@app.route('/')
+@app.route("/")
 def first_page():
     return "api example = www.skyppy.io/api?url=www.youtube.com/watch?v={{youtube_id}}"
+
 
 @app.route("/api", methods=["get", "post"])
 def api():
     return Skyppy_flask.process(request, make_response, Segment, jsonify, Cache)
-    
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7001, threaded=False)
 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7001, threaded=False)
