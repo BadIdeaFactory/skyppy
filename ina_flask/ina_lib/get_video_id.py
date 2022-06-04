888c8469fd436ffb7d2653a301f9df5bd60332bd
Marco Scarselli <scarselli@gmail.com>
2022-06-04 09:20:40 +0200

    refactor skyppy_core.py

--- ina_flask/ina_lib/get_video_id.py
+++ ina_flask/ina_lib/get_video_id.py
@@ -1,13 +1,27 @@
 import youtube_dl
 
+
 def get_video_id(url):
     ydl_opts = {
-                "format": "bestaudio/best",
-            }
+        "format": "bestaudio/best",
+    }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            metadata = ydl.extract_info(url, download=False)["id"]
+        metadata = ydl.extract_info(url, download=False)["id"]
 
     return metadata
 
+
 if __name__ == "__main__":
     assert get_video_id("https://www.youtube.com/watch?v=OG__SwkV3wg") == "OG__SwkV3wg"
+
+
+def get_youtube_id_from_request(request) -> dict:
+    posted = {}
+    posted["link_video"] = "https://" + request.args.get("url", default="*", type=str)
+    id_youtube_file = (
+        request.args.get("url", default="*", type=str).split("?v=")[1] + ".json"
+    )
+    id_youtube = request.args.get("url", default="*", type=str).split("?v=")[1]
+    posted["id_youtube"] = id_youtube
+    posted["id_youtube_file"] = id_youtube_file
+    return posted
--- ina_flask/skyppy_core.py
+++ ina_flask/skyppy_core.py
@@ -8,7 +8,7 @@ import config
 from ina_lib.audio_segmenter import AudioSegmenter
 from ina_lib.check_video_lenght import CheckVideoLenght
 from ina_lib.download_audio import DownloadAudio
-from ina_lib.get_video_id import get_video_id
+from ina_lib.get_video_id import get_video_id, get_youtube_id_from_request
 from ina_lib.status import Status
 from ina_tools import segmentation_to_json
 
@@ -53,24 +53,16 @@ class Skyppy_flask:
         Cache,
         video_lengt_in_minutes: int = config.option.max_video_lenght_in_minutes,
     ):
-        posted = {}
-        posted["link_video"] = "https://" + request.args.get(
-            "url", default="*", type=str
-        )
-        id_youtube_file = (
-            request.args.get("url", default="*", type=str).split("?v=")[1] + ".json"
-        )
-        id_youtube = request.args.get("url", default="*", type=str).split("?v=")[1]
-
+        posted = get_youtube_id_from_request(request)
         try:
             video_lenght = CheckVideoLenght().get(posted["link_video"])
-        except Exception as e:
+        except youtube_dl.utils.DownloadError as e:
             payload = jsonify(
                 {
                     "lib": "error: Video unavailable - wrong video url or your ip is banned from youtube",
                     "data": "error",
                     "status_code": 404,
-                    "error": str(e)
+                    "error": str(e),
                 }
             )
             resp = make_response(payload, 404)
@@ -89,60 +81,46 @@ class Skyppy_flask:
             )
             return result
 
-        status = Status(id_youtube)
-        try:
-            cache = Cache(id_youtube_file)
+        status = Status(posted["id_youtube"])
 
-            if cache.check_log():
-                result = cache.data_from_log()
-                return result
+        cache = Cache(posted["id_youtube_file"])
 
-            # if request.args.get("noprocess", default=False, type=bool):
-            #     return jsonify(False)
-
-            segment = Segment(posted)
-            try:
-                if status.download():
-                    segment.download()
-                else:
-                    payload = jsonify(
-                        {
-                            "lib": """processing {id_youtube_file}""",
-                            "data": "please wait",
-                            "status_code": 102,
-                        }
-                    )
-                    resp = make_response(payload, 102)
-                    result = resp
-                    return result
-
-            except:
-                payload = jsonify(
-                    {"lib": "error_in_download", "data": "error", "status_code": 404}
-                )
-                resp = make_response(payload, 404)
-                result = resp
-                return result
-            status.segmenter()
-            video_segment = segment.segmenter()
-            video_segment["embed"] = "https://www.youtube.com/embed/{}".format(
-                segment.video_id
-            )
-            output = jsonify(video_segment)
-            if video_segment["embed"] != "error":
-                cache.save_from_log(video_segment["embed"], video_segment["data"])
-            result = output, 200
-            status.complete(video_segment["data"])
-        except:
-            payload = jsonify(
-                {"lib": "audio_segmenter", "data": "error", "status_code": 404}
-            )
-            resp = make_response(payload, 404)
-            result = resp
-
-        finally:
-            for item in os.listdir():
-                if item[-3:] == "mp3":
-                    os.remove(item)
-
+        if cache.check_log():
+            result = cache.data_from_log()
             return result
+
+        # if request.args.get("noprocess", default=False, type=bool):
+        #     return jsonify(False)
+
+        segment = Segment(posted)
+
+        if status.download():
+            segment.download()
+        else:
+            payload = jsonify(
+                {
+                    "lib": """processing {id_youtube_file}""",
+                    "data": "please wait",
+                    "status_code": 102,
+                }
+            )
+            resp = make_response(payload, 102)
+            result = resp
+            return result
+
+        status.segmenter()
+        video_segment = segment.segmenter()
+        video_segment["embed"] = "https://www.youtube.com/embed/{}".format(
+            segment.video_id
+        )
+        output = jsonify(video_segment)
+        if video_segment["embed"] != "error":
+            cache.save_from_log(video_segment["embed"], video_segment["data"])
+        result = output, 200
+        status.complete(video_segment["data"])
+
+        for item in os.listdir():
+            if item[-3:] == "mp3":
+                os.remove(item)
+
+        return result
