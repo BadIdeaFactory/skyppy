ec023db3d5d6bd7d7e3e6ed403e9919798d20230
Marco Scarselli <scarselli@gmail.com>
2022-11-22 10:04:43 +0100

    add poetry install

--- codespaces.sh
+++ codespaces.sh
@@ -1,4 +1,5 @@
-sudo apt get update && apt get install -y gcc libpq-dev libsndfile-dev ffmpeg
+sudo apt update && sudo apt install -y gcc libpq-dev libsndfile-dev ffmpeg
 curl -sSL https://install.python-poetry.org | python3 -
+poetry install
 poetry shell
 python run_local.py
